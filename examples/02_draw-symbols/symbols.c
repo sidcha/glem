6893e6902a26bcbf576a575f8f90f19f25f6bd68
Siddharth Chandrasekaran <siddharth@embedjournal.com>
2018-05-06 19:33:39 +0530

    Switch to MIT license; fix source banner inconsistencies

--- examples/02_draw-symbols/symbols.c
+++ examples/02_draw-symbols/symbols.c
@@ -1,3 +1,31 @@
+/******************************************************************************
+
+                  Copyright (c) 2017 Siddharth Chandrasekaran
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+    Author : Siddharth Chandrasekaran
+    Email  : siddharth@embedjournal.com
+    Date   : 07-May-2016
+
+******************************************************************************/
+
 #include "symbols.h"
 
 /* 
@@ -67,5 +95,4 @@ const symbol_t glem_logo = {
 	OBJ_ROW_MAJOR,
 	glem_logo_bmp
 };
-
 
